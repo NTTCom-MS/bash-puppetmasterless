e63e6a7b9b4d6c6b4e29c99afd5d4e42e5bfd0c3
Jordi Prats <jordi.prats@gmail.com>
2020-08-12 16:24:01 +0200

    canvi install

--- setup.sh
+++ setup.sh
@@ -1,59 +1,5 @@
 #!/bin/bash
 
-gather_facts()
-{
-  FACTERBIN=$(which facter 2>/dev/null)
-  if [ -z "$FACTERBIN" ] || [ -z "$(facter -p operatingsystemmajrelease 2>/dev/null)" ];
-  then
-    if [ -e "/etc/redhat-release" ];
-    then
-      FACT_OSFAMILY="RedHat"
-
-      FACT_OPERATINGSYSTEM="RedHat"
-
-      FACT_OPERATINGSYSTEMRELEASE=$(grep -Eo "[0-9]*\.[0-9]*" /etc/redhat-release)
-
-      FACT_OPERATINGSYSTEMMAJRELEASE=$(grep -Eo "[0-9]*\.[0-9]*" /etc/redhat-release | cut -f1 -d.)
-
-    elif [ -e "/etc/SuSE-release" ];
-    then
-      FACT_OSFAMILY="Suse"
-
-      FACT_OPERATINGSYSTEM="SLES"
-
-      FACT_OPERATINGSYSTEMRELEASE="$(grep VERSION /etc/SuSE-release | awk '{ print $NF }').$(grep PATCHLEVEL /etc/SuSE-release | awk '{ print $NF }')"
-
-      FACT_OPERATINGSYSTEMMAJRELEASE="$(grep VERSION /etc/SuSE-release | awk '{ print $NF }')"
-    elif [ -e "/etc/lsb-release" ];
-    then
-
-      FACT_OPERATINGSYSTEM=$(grep DISTRIB_ID /etc/lsb-release | cut -f2 -d=)
-
-      if [ "${FACT_OPERATINGSYSTEM}" == "Ubuntu" ];
-      then
-        FACT_OSFAMILY="Debian"
-
-        FACT_OPERATINGSYSTEMRELEASE="$(grep DISTRIB_RELEASE /etc/lsb-release | cut -f2 -d=)"
-
-        FACT_OPERATINGSYSTEMMAJRELEASE="$(grep DISTRIB_RELEASE /etc/lsb-release | cut -f2 -d=)"
-
-        FACT_LSBDISTCODENAME="$(grep DISTRIB_CODENAME /etc/lsb-release | cut -f2 -d=)"
-      fi
-
-    fi
-  else
-    FACT_OSFAMILY=$(facter -p osfamily)
-
-    FACT_OPERATINGSYSTEM=$(facter -p operatingsystem)
-
-    FACT_OPERATINGSYSTEMRELEASE=$(facter -p operatingsystemrelease)
-
-    FACT_OPERATINGSYSTEMMAJRELEASE=$(facter -p operatingsystemmajrelease)
-
-    FACT_LSBDISTCODENAME=$(facter -p lsbdistcodename)
-  fi
-}
-
 prepostinstall_checks()
 {
   if [ "${FACT_OSFAMILY}" == "RedHat" ];
@@ -113,43 +59,6 @@ prepostinstall_checks()
 
 }
 
-puppet_install()
-{
-  if [ "${FACT_OSFAMILY}" == "RedHat" ];
-  then
-    rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-${FACT_OPERATINGSYSTEMMAJRELEASE}.noarch.rpm
-    $PKG_INSTALL $PKG_INSTALL_UNATTENDED puppet-agent
-  elif [ "${FACT_OSFAMILY}" == "Debian" ];
-  then
-    if [ "${FACT_OPERATINGSYSTEMMAJRELEASE}" != "18.04" ];
-    then
-      wget https://apt.puppetlabs.com/puppet5-release-${FACT_LSBDISTCODENAME}.deb
-      dpkg -i puppet5-release-${FACT_LSBDISTCODENAME}.deb
-      apt-get update
-    fi
-    $PKG_INSTALL $PKG_INSTALL_UNATTENDED puppet-agent
-  fi
-
-  source /etc/profile.d/puppet-agent.sh
-
-  puppet --version >/dev/null 2>&1
-
-  if [ "$?" -ne 0 ];
-  then
-    echo "error installing puppet"
-    exit 0
-  fi
-
-  echo "PUPPET VERSION: $(puppet --version)"
-
-  /opt/puppetlabs/puppet/bin/gem install r10k
-
-  # bug 930 - https://github.com/puppetlabs/r10k/issues/930
-  /opt/puppetlabs/puppet/bin/gem  install cri --version 2.15.6
-  /opt/puppetlabs/puppet/bin/gem  uninstall cri --version 2.15.7
-
-}
-
 puppet_check()
 {
   PUPPET_FULL_VERSION=$(puppet --version 2>/dev/null)
@@ -175,6 +84,97 @@ puppet_check()
     exit 1
   fi
 }
+
+gather_facts()
+{
+  FACTERBIN=$(which facter 2>/dev/null)
+  if [ -z "$FACTERBIN" ] || [ -z "$(facter -p operatingsystemmajrelease 2>/dev/null)" ];
+  then
+    if [ -e "/etc/redhat-release" ];
+    then
+      FACT_OSFAMILY="RedHat"
+
+      FACT_OPERATINGSYSTEM="RedHat"
+
+      FACT_OPERATINGSYSTEMRELEASE=$(grep -Eo "[0-9]*\.[0-9]*" /etc/redhat-release)
+
+      FACT_OPERATINGSYSTEMMAJRELEASE=$(grep -Eo "[0-9]*\.[0-9]*" /etc/redhat-release | cut -f1 -d.)
+
+    elif [ -e "/etc/SuSE-release" ];
+    then
+      FACT_OSFAMILY="Suse"
+
+      FACT_OPERATINGSYSTEM="SLES"
+
+      FACT_OPERATINGSYSTEMRELEASE="$(grep VERSION /etc/SuSE-release | awk '{ print $NF }').$(grep PATCHLEVEL /etc/SuSE-release | awk '{ print $NF }')"
+
+      FACT_OPERATINGSYSTEMMAJRELEASE="$(grep VERSION /etc/SuSE-release | awk '{ print $NF }')"
+    elif [ -e "/etc/lsb-release" ];
+    then
+
+      FACT_OPERATINGSYSTEM=$(grep DISTRIB_ID /etc/lsb-release | cut -f2 -d=)
+
+      if [ "${FACT_OPERATINGSYSTEM}" == "Ubuntu" ];
+      then
+        FACT_OSFAMILY="Debian"
+
+        FACT_OPERATINGSYSTEMRELEASE="$(grep DISTRIB_RELEASE /etc/lsb-release | cut -f2 -d=)"
+
+        FACT_OPERATINGSYSTEMMAJRELEASE="$(grep DISTRIB_RELEASE /etc/lsb-release | cut -f2 -d=)"
+
+        FACT_LSBDISTCODENAME="$(grep DISTRIB_CODENAME /etc/lsb-release | cut -f2 -d=)"
+      fi
+
+    fi
+  else
+    FACT_OSFAMILY=$(facter -p osfamily)
+
+    FACT_OPERATINGSYSTEM=$(facter -p operatingsystem)
+
+    FACT_OPERATINGSYSTEMRELEASE=$(facter -p operatingsystemrelease)
+
+    FACT_OPERATINGSYSTEMMAJRELEASE=$(facter -p operatingsystemmajrelease)
+
+    FACT_LSBDISTCODENAME=$(facter -p lsbdistcodename)
+  fi
+}
+
+
+puppet_install()
+{
+  if [ "${FACT_OSFAMILY}" == "RedHat" ];
+  then
+    rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-${FACT_OPERATINGSYSTEMMAJRELEASE}.noarch.rpm
+    $PKG_INSTALL $PKG_INSTALL_UNATTENDED puppet-agent
+  elif [ "${FACT_OSFAMILY}" == "Debian" ];
+  then
+    if [ "${FACT_OPERATINGSYSTEMMAJRELEASE}" != "18.04" ];
+    then
+      wget https://apt.puppetlabs.com/puppet5-release-${FACT_LSBDISTCODENAME}.deb
+      dpkg -i puppet5-release-${FACT_LSBDISTCODENAME}.deb
+      apt-get update
+    fi
+    $PKG_INSTALL $PKG_INSTALL_UNATTENDED puppet-agent
+  elif [ "${FACT_OSFAMILY}" == "Suse" ];
+    echo "SuSE Unsupported"
+    exit 1
+  fi
+
+  source /etc/profile.d/puppet-agent.sh
+
+  puppet --version >/dev/null 2>&1
+
+  if [ "$?" -ne 0 ];
+  then
+    echo "error installing puppet"
+    exit 0
+  fi
+
+  echo "PUPPET VERSION: $(puppet --version)"
+
+  /opt/puppetlabs/puppet/bin/gem install r10k
+}
+
 
 
 gather_facts
